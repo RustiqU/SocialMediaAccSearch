e0634026fd010eff930e7e2dbf2f4cad0db0d18a
RustiqU <106302653+RustiqU@users.noreply.github.com>
2025-11-21 19:04:36 +0100

    Update search queries to use OR instead of AND

--- Social Media account searcher.py
+++ Social Media account searcher.py
@@ -8,16 +8,16 @@ base_params = {
 }
 
 queries = {
-    "Twitter": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:twitter.com",
-    "Facebook": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:facebook.com",
-    "LinkedIn": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:linkedin.com",
-    "YouTube": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:youtube.com",
-    "TikTok": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:tiktok.com",
-    "Pinterest": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:pinterest.com",
-    "Medium": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:medium.com",
-    "Twitch": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:twitch.tv",
-    "Vimeo": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:vimeo.com",
-    "Instagram": "Keyword 1 2024 OR Keyword 2 OR Keyword 3 site:instagram.com"
+    "Twitter": "Keyword 1 OR Keyword 2 OR Keyword 3 site:twitter.com",
+    "Facebook": "Keyword 1 OR Keyword 2 OR Keyword 3 site:facebook.com",
+    "LinkedIn": "Keyword 1 OR Keyword 2 OR Keyword 3 site:linkedin.com",
+    "YouTube": "Keyword 1 OR Keyword 2 OR Keyword 3 site:youtube.com",
+    "TikTok": "Keyword 1 OR Keyword 2 OR Keyword 3 site:tiktok.com",
+    "Pinterest": "Keyword 1 OR Keyword 2 OR Keyword 3 site:pinterest.com",
+    "Medium": "Keyword 1 OR Keyword 2 OR Keyword 3 site:medium.com",
+    "Twitch": "Keyword 1 OR Keyword 2 OR Keyword 3 site:twitch.tv",
+    "Vimeo": "Keyword 1 OR Keyword 2 OR Keyword 3 site:vimeo.com",
+    "Instagram": "Keyword 1 OR Keyword 2 OR Keyword 3 site:instagram.com"
 }
 
 # Otwórz plik CSV i zapisz nagłówki kolumn
@@ -40,3 +40,4 @@ with open('results.csv', mode='w', newline='', encoding='utf-8') as file:
                 writer.writerow([platform, result['title'], result['link'], result['snippet']])
         else:
             writer.writerow([platform, "No results found", "", ""])
+
